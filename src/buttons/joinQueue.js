0f7cc124ef37dd23550023f5ab5b72999f70270e
aspiredzz <cantletmeplease@gmail.com>
2026-07-12 12:29:29 +0800

    Delete src directory

--- src/buttons/joinQueue.js
+++
@@ -1,6 +0,0 @@
-const { joinQueue } = require('../utils/queueActions');
-
-module.exports = {
-  customId: 'join_queue',
-  execute: joinQueue,
-};
--- src/buttons/leaveQueue.js
+++
@@ -1,6 +0,0 @@
-const { leaveQueue } = require('../utils/queueActions');
-
-module.exports = {
-  customId: 'leave_queue',
-  execute: leaveQueue,
-};
